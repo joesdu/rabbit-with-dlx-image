fd11dd74440b63c4a61dad957dc748e9e31bbadd
Joe Du <13188169+joesdu@users.noreply.github.com>
2024-07-24 13:57:34 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,22 @@
 $base = "dygood/" ;
 $name = "rabbitmq-dlx" ;
 
-# 构建 latest
+# 构建 release-tag
 $tag = ":" + $env:RELEASE_TAG;
+$imagename = $base + $name + $tag ;
+$imagename ;
+
+docker buildx create --use ;
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le,linux/s390x -t $imagename . --progress plain --push ;
+
+$gh = "ghcr.io/joesdu/";
+$ghimagename = $gh + $name + $tag ;
+$ghimagename;
+
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le,linux/s390x -t $ghimagename . --progress plain --push ;
+
+# 构建 latest
+$tag = ":latest";
 $imagename = $base + $name + $tag ;
 $imagename ;
 
